b37cc052aae6e2beb6c98534cc2dac87888dbe00
Eduardo Elias <edu.elias2004@gmail.com>
2025-03-06 19:44:07 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+print('Starting game...')
+pygame.init()
+screen = pygame.display.set_mode(size = (600, 480))
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      print('Closing game...') 
+      pygame.quit()
+      quit()
+      
